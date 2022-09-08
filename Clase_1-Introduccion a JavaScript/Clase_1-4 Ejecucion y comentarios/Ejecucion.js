a2998658b402526deb818fa43ba5134952c35671
ThomasI <machuca_01_al@hotmail.com>
2022-09-08 12:19:48 -0500

    Ejercicio 2

---
+++ Clase_1-Introduccion a JavaScript/Clase_1-4 Ejecucion y comentarios/Ejecucion.js
@@ -0,0 +1,9 @@
+/*  Los scripts*/
+
+    /*Para activar el comando debemos poner "npm run start "test"" tambien podemos omitir el start*/
+    /* Son los programas que podamos tener en la carpeta*/
+        /*ejemplo:  */
+        /*saludar: node hola_mundo.js */
+        /*Saludar es el scripts */
+        /*"node hola_mundo.js" es el nombre del archivo que queremos   
+            ejecutar */
