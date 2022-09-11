af4c0004683e8785c879b20fc237dd02c406c56d
ThomasI <machuca_01_al@hotmail.com>
2022-09-11 12:47:30 -0500

    Ejercicio 2

---
+++ Ejercicio-2 Sintaxis,variables y palabras de JS/SintaxisVariables.js
@@ -0,0 +1,20 @@
+let tu_nombre="Diego" ;
+let edad=22;
+let eres_desarrollador=true;
+const cumpleaños=new Date("15 dec  1999") ;
+const autor_fecha=new Date("15 april 1977") ;
+
+const book={
+    titulo:"La tia julia y el escribidor",
+    autor:"Mario Vargas LLosa",
+    Publicacion: autor_fecha,
+    url: href="https://www.amazon.com/-/es/Mario-Vargas-Llosa/dp/9681906888",
+}
+
+
+
+const lista={tu_nombre, edad, eres_desarrollador,cumpleaños, book }
+
+
+console.log(lista) ;
+
