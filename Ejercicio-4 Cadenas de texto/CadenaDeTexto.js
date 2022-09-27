5f4344fd709e72b73088799a9f2cffb9199709f7
ThomasI <machuca_01_al@hotmail.com>
2022-09-27 11:17:47 -0500

    Ejercicio 4

---
+++ Ejercicio-4 Cadenas de texto/CadenaDeTexto.js
@@ -0,0 +1,26 @@
+let nombre = "Diego";
+
+let apellido ="Laura" ;
+
+let cadena_1 = `Estudiante ${nombre} ${apellido}`;
+
+let estudianteMayus = 'estudiante' ;
+
+console.log(estudianteMayus.toUpperCase()) ;
+
+let estudianteMinus = "estudiante" 
+
+console.log(estudianteMinus.toLowerCase());
+
+let cadena_2 = "  estudiante ";
+
+console.log(cadena_2.length);
+
+console.log(nombre.charAt(0));
+
+console.log(apellido.length);
+console.log(apellido.charAt(4));
+
+console.log(cadena_2.trim())
+
+console.log(cadena_2.includes("diego"))
