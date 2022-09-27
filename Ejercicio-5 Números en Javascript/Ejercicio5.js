f2e1a2a214c44c9cefcb6ea7eb5272c029645edc
ThomasI <machuca_01_al@hotmail.com>
2022-09-27 17:21:08 -0500

    Ejercicio 5

---
+++ Ejercicio-5 Números en Javascript/Ejercicio5.js
@@ -0,0 +1,11 @@
+let alturaCm = 169;
+let alturaFloat = 1.69;
+let pesoKg = 80.40;
+
+let valorMinimo= Number.MAX_VALUE
+let valorMasUno = Number.MAX_VALUE +1
+let resultado = valorMinimo == valorMasUno
+
+console.log(alturaCm.toFixed(0));
+console.log(pesoKg.toFixed(0));
+console.log(resultado);
